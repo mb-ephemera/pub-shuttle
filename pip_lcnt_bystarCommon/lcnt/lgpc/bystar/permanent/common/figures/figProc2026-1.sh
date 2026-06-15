f05a4e0679de8f2dd15d224bf92321d535a06243
Mohsen Banan <form@mohsen.1.banan.byname.net>
2026-06-15 15:18:23 -0700

    Added Acts 3 and 4.

---
+++ pip_lcnt_bystarCommon/lcnt/lgpc/bystar/permanent/common/figures/figProc2026-1.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# typeset RcsId="$Id: figProc.sh,v 1.12 2018-12-09 07:34:54 lsipusr Exp $"
+
+# if [ "${loadFiles}X" == "X" ] ; then
+#   seedFigProc.sh -l $0 "$@"
+#   exit $?
+# fi
+
+currentBase=`pwd`
+
+####+BEGIN: bx:bisos:bash:seed-spec :types "/bisos/core/lcnt/bin/seedFigProc.sh"
+SEED="
+*  /[dblock]/ /Seed/ :: [[file:/bisos/core/lcnt/bin/seedFigProc.sh]] |
+"
+FILE="
+*  /This File/ :: /bxo/r3/iso/pip_lcnt_bystarCommon/lcnt/lgpc/bystar/permanent/common/figures/figProc.sh
+"
+if [ "${loadFiles}" == "" ] ; then
+    /bisos/core/lcnt/bin/seedFigProc.sh -l $0 "$@"
+    exit $?
+fi
+####+END:
+
+
+####+BEGINNOT: bx:dblock:lcnt:bash:figsList :types "odg"
+filesList="
+bx-containment.odg
+pycsFramework.odg
+"
+####+END:
+
+figsList="${filesList}"
+
+FullfigsList="
+${figsList}
+"
+
+####+BEGIN: bx:dblock:bash:end-of-file :types ""
+# {{{ DBLOCK-end-of-file
+#local variables:
+#major-mode: shellscript-mode
+#folded-file: nil
+#fill-column: 90
+# end:
+# }}} DBLOCK-end-of-file
+####+END:
